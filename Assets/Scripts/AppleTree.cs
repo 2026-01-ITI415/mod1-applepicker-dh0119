7497f80b9ce10ca88d35c712fc17a4eab410a691
dh0119 <th692@scarletmail.rutgers.edu>
2026-02-06 03:41:19 -0500

    v4

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -23,12 +23,40 @@ public class AppleTree : MonoBehaviour
     void Start()
     {
         //Dropping apples every second
+        Invoke("DropApple", 2f );
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
     }
 
     // Update is called once per frame
     void Update()
     {
         //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
         //Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);//Move right
+        } else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);//Move left
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Changing dir randomly time-based
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;//Change directions
+        }
     }
 }
